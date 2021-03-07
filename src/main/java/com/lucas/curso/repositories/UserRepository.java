5dd63959dc2b975e3858709a439de0d9a7958b30
lucassantoss1701 <lucassantoss1701@gmail.com>
2021-03-07 13:35:52 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/lucas/curso/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.lucas.curso.repositories;
+
+import com.lucas.curso.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
--- src/main/java/com/lucas/curso/resources/UserResource.java
+++ src/main/java/com/lucas/curso/resources/UserResource.java
@@ -12,7 +12,7 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<User> findAll(){
-        User user = new User(1L, "Maria", "Maria@gmail.com", "19995776598", "1537928154");
+        User user = new User(1L, "LHFEWYFU", "Maria@gmail.com", "19995776598", "1537928154");
         return ResponseEntity.ok().body(user);
     }
 
